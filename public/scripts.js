f0dba369162da2773849fd3d8253c2637a05489a
Cassio Faleiro <cassio.faleiro@icloud.com>
2020-07-18 23:20:46 -0300

    finished

--- public/scripts.js
+++ public/scripts.js
@@ -15,10 +15,10 @@ const buttons = document.querySelectorAll('.button')
 for (let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", function(){
         if(buttons[i].innerHTML == 'Esconder'){
-            hide[i].classList.add('active')
+            hide[i].classList.remove('active')
             buttons[i].innerHTML = 'Mostrar'
         }else{
-            hide[i].classList.remove('active')
+            hide[i].classList.add('active')
             buttons[i].innerHTML = 'Esconder'
         }
     })
